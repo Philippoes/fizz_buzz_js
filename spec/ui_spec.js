2400f1840baa40c6f8332e5540c784eb033afdc6
Philip <Philip.zdb@gmail.com>
2016-11-24 18:03:12 +0100

    Adds UI spec for testing input

--- spec/ui_spec.js
+++ spec/ui_spec.js
@@ -1,29 +1,19 @@
-describe('Partial sample', function() {
+describe('UI - Index', function() {
   beforeEach(function() {
-    // We can load fixtures using this method:
-    //jasmine.getFixtures().clearCache();
-    //var f = jasmine.getFixtures();
-    //f.fixturesPath = 'base/spec/fixtures';
-    //f.load('sample_partial.html');
-    //$.holdReady(false);
-
-    // Or this:
     jasmine.getFixtures().fixturesPath = 'base/spec/fixtures';
-    loadFixtures('sample_partial.html');
+    loadFixtures('index.html');
     $.holdReady(false);
   });
 
   afterEach(function() {
     // If you need to reset some values after each testing
-    // you can do it here. 
+    // you can do it here.
   });
 
-  describe("displays text", function() {
-
-    it("when button is clicked", function() {
-      $('#text').val('Some random text...');
-      $('#click_me').trigger('click');
-      expect($('#display_message').text()).toBe('Some random text...');
+  describe('It should be able to take an input', function() {
+    it('of number 3', function(){
+      $('#fizzBuzzInput').val(3);
+      expect($('#fizzBuzzInput').val()).toEqual ('3');
     });
   });
 });
